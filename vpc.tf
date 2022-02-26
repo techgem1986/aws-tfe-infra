9d079d7ace576ed169a0a4a9c0341fc287a40fdb
Sandipan Sarkar <work.sandipan@gmail.com>
2022-02-26 18:35:53 +0530

    https://techgem1986-jira.atlassian.net/browse/TG001-4

--- vpc.tf
+++ vpc.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "public-subnet" {
 resource "aws_subnet" "private-subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = "false"
   availability_zone       = "us-east-1b"
 
   tags = {

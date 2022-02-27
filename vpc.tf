011e1d29b5ca395b58fb94cb243a805c9b693807
Sandipan Sarkar <work.sandipan@gmail.com>
2022-02-27 18:26:20 +0530

    https://techgem1986-jira.atlassian.net/browse/TG001-4

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
   version = "~> 3.0"
   name = local.name
   cidr = "10.0.0.0/16"
-  azs             = [data.aws_availability_zones.availability-zones[0], data.aws_availability_zones.availability-zones[1]]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.11.0/24", "10.0.12.0/24"]
   enable_nat_gateway = false
